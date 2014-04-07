ed4c7c0cafed25bba3d448b2f8f176c987be1db5
Sovit Tamrakar <sovit.tamrakar@gmail.com>
2014-04-07 15:00:53 +0545

    - Upgraded to version 3.2.1

--- README.md
+++ README.md
@@ -1,8 +1,32 @@
-## Featured Post WordPress Plugin
+Featured Post WordPress Plugin
+=============
+
 Plugin for featured wordpress posts. This is a cool plugin that makes it easier to mark posts as featured posts (not using specific categories) and simple markup to show theme from your theme file.
 
-Add _**<?php queryposts($querystring."&featured=yes"); ?>**_ before the post loop starts and manage the featured posts from the post edit list.
+Add <?php query_posts($query_string."&featured=yes"); ?> before the post loop starts and manage the featured posts from the post edit list.
 
 Now added widget for listing featured post in sidebar widgets with custom number of post.
 
-_Supports Custom Post Type_
+Features
+=============
+
+- Easy manage featured posts from posts list
+- Filter Featured posts
+- Supports Post/Page and all custom post types
+- Easy to use on theme, or use it as widget for widget areas
+- Ajax powered featured status toggle
+- Dashicons used for featured posts column
+
+
+To-Do
+============
+
+- Slider integration for featured post (still waiting for good slider or a contributer to work on this)
+- Widget for more advanced settings
+- and it still goes on
+
+
+Support & Donate
+=============
+
+Star this repository to donate on flattr, or personally contact me at sovit@ssovit.com to buy me a beer :)
--- featured-post.php
+++ featured-post.php
@@ -1,12 +1,13 @@
 <?php
+
 /*************************************************************************
 
 Plugin Name: Featured Post
 Plugin URI: http://ssovit.com/featured-post-wordpress-plugin/
 Description: Featured Post Plugin For Wordpress.
-Version: 2.0.1
+Version: 3.2.1
 Author: Sovit Tamrakar
-Author URI: http://ssovit.com
+Author URI: http://wppress.net
 
 **************************************************************************
 
@@ -26,311 +27,269 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 **************************************************************************/
-class SovitFeaturedPost
+class Featured_Post
 {
-	var $db = NULL;
-	var $ignore_post_type = array( 'page', 'attachment', 'revision', 'nav_menu_item' );
-	function __construct( )
-	{
-	}
-	function init( )
-	{
-		add_filter( 'query_vars', array(
-			 &$this,
-			'query_vars' 
-		) );
-		add_action( 'pre_get_posts', array(
-			 &$this,
-			'pre_get_posts' 
-		) );
-	}
-	function admin_init( )
-	{
-		add_filter( 'current_screen', array(
-			 &$this,
-			'my_current_screen' 
-		) );
-		add_filter( 'manage_posts_columns', array(
-			 &$this,
-			'manage_posts_columns' 
-		) );
-		add_filter( 'manage_pages_columns', array(
-			 &$this,
-			'manage_posts_columns' 
-		) );
-		add_filter( 'manage_posts_custom_column', array(
-			 &$this,
-			'manage_posts_custom_column' 
-		), 10, 2 );
-		add_filter( 'manage_pages_custom_column', array(
-			 &$this,
-			'manage_posts_custom_column' 
-		), 10, 2 );
-		add_action( 'admin_head-edit.php', array(
-			 &$this,
-			'admin_head' 
-		) );
-		add_filter( 'pre_get_posts', array(
-			 &$this,
-			'admin_pre_get_posts' 
-		), 1 );
-	}
-	function add_views_link( $views )
-	{
-		$post_type           = ( ( isset( $_GET[ 'post_type' ] ) && $_GET[ 'post_type' ] != "" ) ? $_GET[ 'post_type' ] : 'post' );
-		$count               = $this->total_featured( $post_type );
-		$class               = $_GET[ 'post_status' ] == 'featured' ? "current" : '';
-		$views[ 'featured' ] = "<a class=\"$class\" href=\"edit.php?&post_status=featured&post_type={$post_type}\">Featured <span class=\"count\">({$count})</span></a>";
-		return $views;
-	}
-	function total_featured( $post_type = "post" )
-	{
-		$rowQ = new WP_Query( array(
-			 'post_type' => $post_type,
-			'meta_query' => array(
-				 array(
-					 'key' => '_post_is_featured',
-					'value' => 'yes' 
-				) 
-			),
-			'posts_per_page' => 1 
-		) );
-		wp_reset_postdata();
-		wp_reset_query();
-		$rows = $rowQ->found_posts;
-		unset( $rowQ );
-		return $rows;
-	}
-	function my_current_screen( $screen )
-	{
-		if ( defined( 'DOING_AJAX' ) && DOING_AJAX ) {
-			return $screen;
-		}
-		$post_types = get_post_types( '', 'names' );
-		foreach ( $post_types as $post_type ) {
-			if ( !in_array( $post_type, $this->ignore_post_type ) ) {
-				add_filter( 'views_edit-' . $post_type, array(
-					 &$this,
-					'add_views_link' 
-				) );
-			}
-		}
-		return $screen;
-	}
-	function manage_posts_columns( $columns )
-	{
-		global $current_user;
-		get_currentuserinfo();
-		if ( $current_user->allcaps[ 'level_7' ] ) {
-			$columns[ 'featured' ] = __( 'Featured' );
-		}
-		return $columns;
-	}
-	function manage_posts_custom_column( $column_name, $post_id )
-	{
-		//echo "here";
-		if ( $column_name == 'featured' ) {
-			$is_featured = get_post_meta( $post_id, 'featured', true );
-			$class       = "featured-post-toggle";
-			$text        = "NO";
-			if ( $is_featured == "yes" ) {
-				$class .= " featured-post-on";
-				$text = "YES";
-			} else {
-				$class .= " featured-post-off";
-			}
-			echo "<a href=\"#!featured-toggle\" class=\"{$class}\" data-post-id=\"{$post_id}\">$text</a>";
-		}
-	}
-	function admin_head( )
-	{
-		echo '<style>.featured-post-toggle{width:16px; height:16px; display:inline-block; overflow:hidden; text-indent:-1000px;}';
-		echo '.featured-post-off, .featured-post-on:hover{ background:url(http://cdn3.iconfinder.com/data/icons/woothemesiconset/16/star_off.png);}';
-		echo '.featured-post-on, .featured-post-off:hover{ background:url(http://cdn3.iconfinder.com/data/icons/woothemesiconset/16/star.png); }';
-		echo '</style>';
-		echo '<script type="text/javascript">jQuery(document).ready(function($){$(\'.featured-post-toggle\').click(function(e){e.preventDefault();var post_id=$(this).attr(\'data-post-id\');var data={action:\'toggle-featured-post\',post_id:post_id};$.post(ajaxurl,data,function(data){document.location=document.location;})});});</script>';
-	}
-	function admin_ajax( )
-	{
-		$post_id     = $_POST[ 'post_id' ];
-		$is_featured = get_post_meta( $post_id, 'featured', true );
-		$newStatus   = $is_featured == 'yes' ? 'no' : 'yes';
-		delete_post_meta( $post_id, 'featured' );
-		add_post_meta( $post_id, 'featured', $newStatus );
-		echo ":P";
-		die( );
-	}
-	function admin_pre_get_posts( $query )
-	{
-		global $wp_query;
-		if ( is_admin() && $_GET[ 'post_status' ] == 'featured' ) {
-			$query->set( 'meta_key', 'featured' );
-			$query->set( 'meta_value', 'yes' );
-		}
-		return $query;
-	}
-	function query_vars( $public_query_vars )
-	{
-		$public_query_vars[ ] = 'featured';
-		return $public_query_vars;
-	}
-	function pre_get_posts( $query )
-	{
-		if ( !is_admin() ) {
-			if ( $query->get( 'featured' ) == 'yes' ) {
-				$query->set( 'meta_key', 'featured' );
-				$query->set( 'meta_value', 'yes' );
-			}
-		}
-		return $query;
-	}
+    var $db = NULL;
+    public $post_types = array();
+    
+    function __construct() {
+        
+        add_action('init', array(&$this,
+            'init'
+        ));
+        add_action('admin_init', array(&$this,
+            'admin_init'
+        ));
+        add_action('wp_ajax_toggle-featured-post', array(&$this,
+            'admin_ajax'
+        ));
+    }
+    function init() {
+        
+        add_filter('query_vars', array(&$this,
+            'query_vars'
+        ));
+        add_action('pre_get_posts', array(&$this,
+            'pre_get_posts'
+        ));
+    }
+    function admin_init() {
+        add_filter('current_screen', array(&$this,
+            'my_current_screen'
+        ));
+        
+        add_action('admin_head-edit.php', array(&$this,
+            'admin_head'
+        ));
+        add_filter('pre_get_posts', array(&$this,
+            'admin_pre_get_posts'
+        ) , 1);
+        $this->post_types = get_post_types(array(
+            '_builtin' => false,
+        ) , 'names', 'or');
+        $this->post_types['post'] = 'post';
+        $this->post_types['page'] = 'page';
+        ksort($this->post_types);
+        foreach ($this->post_types as $key => $val) {
+            add_filter('manage_edit-' . $key . '_columns', array(&$this,
+                'manage_posts_columns'
+            ));
+            add_action('manage_' . $key . '_posts_custom_column', array(&$this,
+                'manage_posts_custom_column'
+            ) , 10, 2);
+        }
+    }
+    function add_views_link($views) {
+        $post_type = ((isset($_GET['post_type']) && $_GET['post_type'] != "") ? $_GET['post_type'] : 'post');
+        $count = $this->total_featured($post_type);
+        $class = $_GET['post_status'] == 'featured' ? "current" : '';
+        $views['featured'] = "<a class=\"" . $class . "\" id=\"featured-post-filter\" href=\"edit.php?&post_status=featured&post_type={$post_type}\">Featured <span class=\"count\">({$count})</span></a>";
+        return $views;
+    }
+    function total_featured($post_type = "post") {
+        $rowQ = new WP_Query(array(
+            'post_type' => $post_type,
+            'meta_query' => array(
+                array(
+                    'key' => '_is_featured',
+                    'value' => 'yes'
+                )
+            ) ,
+            'posts_per_page' => 1
+        ));
+        wp_reset_postdata();
+        wp_reset_query();
+        $rows = $rowQ->found_posts;
+        unset($rowQ);
+        return $rows;
+    }
+    function my_current_screen($screen) {
+        if (defined('DOING_AJAX') && DOING_AJAX) {
+            return $screen;
+        }
+        $this->post_types = get_post_types(array(
+            '_builtin' => false,
+        ) , 'names', 'or');
+        $this->post_types['post'] = 'post';
+        $this->post_types['page'] = 'page';
+        ksort($this->post_types);
+        foreach ($this->post_types as $key => $val) {
+            add_filter('views_edit-' . $key, array(&$this,
+                'add_views_link'
+            ));
+        }
+        return $screen;
+    }
+    function manage_posts_columns($columns) {
+        global $current_user;
+        get_currentuserinfo();
+        if (current_user_can('edit_posts', $user_id)) {
+            $columns['featured'] = __('Featured');
+        }
+        return $columns;
+    }
+    function manage_posts_custom_column($column_name, $post_id) {
+        
+        //echo "here";
+        if ($column_name == 'featured') {
+            $is_featured = get_post_meta($post_id, '_is_featured', true);
+            $class = "dashicons";
+            $text = "";
+            if ($is_featured == "yes") {
+                $class.= " dashicons-star-filled";
+                $text = "";
+            } else {
+                $class.= " dashicons-star-empty";
+            }
+            echo "<a href=\"#!featured-toggle\" class=\"featured-post-toggle {$class}\" data-post-id=\"{$post_id}\">$text</a>";
+        }
+    }
+    function admin_head() {
+        
+        echo '<script type="text/javascript">
+		jQuery(document).ready(function($){
+			$(\'.featured-post-toggle\').on("click",function(e){
+				e.preventDefault();
+				var _el=$(this);
+				var post_id=$(this).attr(\'data-post-id\');
+				var data={action:\'toggle-featured-post\',post_id:post_id};
+				$.ajax({url:ajaxurl,data:data,type:\'post\',
+					dataType:\'json\',
+					success:function(data){
+					_el.removeClass(\'dashicons-star-filled\').removeClass(\'dashicons-star-empty\');
+					$("#featured-post-filter span.count").text("("+data.total_featured+")");
+					if(data.new_status=="yes"){
+						_el.addClass(\'dashicons-star-filled\');
+					}else{
+						_el.addClass(\'dashicons-star-empty\');
+					}
+					}
+				
+					
+				});
+			});
+		});
+		</script>';
+    }
+    function admin_ajax() {
+        header('Content-Type: application/json');
+        $post_id = $_POST['post_id'];
+        $is_featured = get_post_meta($post_id, '_is_featured', true);
+        $newStatus = $is_featured == 'yes' ? 'no' : 'yes';
+        delete_post_meta($post_id, '_is_featured');
+        add_post_meta($post_id, '_is_featured', $newStatus);
+        echo json_encode(array(
+            'ID' => $post_id,
+            'new_status' => $newStatus,
+            'total_featured' => $this->total_featured(get_post_type($post_id))
+        ));
+        die();
+    }
+    function admin_pre_get_posts($query) {
+        global $wp_query;
+        if (is_admin() && $_GET['post_status'] == 'featured') {
+            $query->set('meta_key', '_is_featured');
+            $query->set('meta_value', 'yes');
+        }
+        return $query;
+    }
+    function query_vars($public_query_vars) {
+        $public_query_vars[] = 'featured';
+        return $public_query_vars;
+    }
+    function pre_get_posts($query) {
+        if (!is_admin()) {
+            if ($query->get('featured') == 'yes') {
+                $query->set('meta_key', '_is_featured');
+                $query->set('meta_value', 'yes');
+            }
+        }
+        return $query;
+    }
 }
-$SovitFeaturedPost = new SovitFeaturedPost();
-add_action( 'init', array(
-	 $SovitFeaturedPost,
-	'init' 
-) );
-add_action( 'admin_init', array(
-	 $SovitFeaturedPost,
-	'admin_init' 
-) );
-add_action( 'wp_ajax_toggle-featured-post', array(
-	 $SovitFeaturedPost,
-	'admin_ajax' 
-) );
 class Featured_Post_Widget extends WP_Widget
 {
-	function __construct( )
-	{
-		parent::WP_Widget( false, $name = 'Featured Post' );
-	}
-	function form( $instance )
-	{
-		global $SovitFeaturedPost;
-		$title      = esc_attr( $instance[ 'title' ] );
-		$type       = esc_attr( $instance[ 'post_type' ] );
-		$num        = (int) esc_attr( $instance[ 'num' ] );
-		$post_types = get_post_types( array(
-			 'publicly_queryable' => true 
-		), 'objects' );
-?>
-
-<p>
-	<label for="<?php
-		echo $this->get_field_id( 'title' );
-?>">
-		<?php
-		_e( 'Title:' );
-?>
-	</label>
-	<input class="widefat" id="<?php
-		echo $this->get_field_id( 'title' );
-?>" name="<?php
-		echo $this->get_field_name( 'title' );
-?>" type="text" value="<?php
-		echo $title;
-?>" />
-</p>
-<p>
-	<label for="<?php
-		echo $this->get_field_id( 'post_type' );
-?>">
-		<?php
-		_e( 'Post Type:' );
-?>
-	</label>
-	<select name="<?php
-		echo $this->get_field_name( 'post_type' );
-?>"  id="<?php
-		echo $this->get_field_id( 'title' );
-?>" >
-		<?php
-		foreach ( $post_types as $key => $post_type ) {
-?>
-		<?php
-			if ( !in_array( $key, $SovitFeaturedPost->ignore_post_type ) ) {
-?>
-		<option value="<?php
-				echo $key;
-?>"<?php
-				echo ( $key == $type ? " selected" : "" );
-?>><?php
-				echo $post_type->labels->name;
-?></option>
-		<?php
-			}
-?>
-		<?php
-		}
-?>
-	</select>
-</p>
-<p>
-	<label for="<?php
-		echo $this->get_field_id( 'num' );
-?>">
-		<?php
-		_e( 'Number To show:' );
-?>
-	</label>
-	<input id="<?php
-		echo $this->get_field_id( 'num' );
-?>" class="widefat" name="<?php
-		echo $this->get_field_name( 'num' );
-?>" type="text" value="<?php
-		echo $num;
-?>" />
-</p>
-<?php
-	}
-	function update( $new_instance, $old_instance )
-	{
-		$instance                = $old_instance;
-		$instance[ 'title' ]     = strip_tags( $new_instance[ 'title' ] );
-		$instance[ 'num' ]       = (int) strip_tags( $new_instance[ 'num' ] );
-		$instance[ 'post_type' ] = strip_tags( $new_instance[ 'post_type' ] );
-		if ( $instance[ 'num' ] < 1 ) {
-			$instance[ 'num' ] = 10;
-		}
-		return $instance;
-	}
-	function widget( $args, $instance )
-	{
-		extract( $args );
-		$title = apply_filters( 'widget_title', $instance[ 'title' ] );
-?>
-<?php
-		echo $before_widget;
-?>
-<?php
-		if ( $title )
-			echo $before_title . $title . $after_title;
-?>
-<ul class="widget-list featured-post-widget featured-post">
-	<?php
-		wp_reset_query();
-		query_posts( 'post_type=' . $instance[ 'post_type' ] . '&showposts=' . $instance[ 'num' ] . '&featured=yes' );
-		while ( have_posts() ):
-			the_post();
-?>
-	<li><a href="<?php
-			the_permalink();
-?>">
-		<?php
-			the_title();
-?>
-		</a></li>
-	<?php
-		endwhile;
-		wp_reset_query(); // set it not to reset the query when in featured posts page of edit.php in wp-admin
-?>
-</ul>
-<?php
-		echo $after_widget;
-?>
-<?php
-		// outputs the content of the widget
-	}
+    private $post_types = array();
+    function __construct() {
+        parent::WP_Widget(false, $name = 'Featured Post');
+    }
+    
+    function form($instance) {
+        $title = esc_attr($instance['title']);
+        $type = esc_attr($instance['post_type']);
+        $num = (int)esc_attr($instance['num']);
+        $this->post_types = get_post_types(array(
+            '_builtin' => false,
+        ) , 'names', 'or');
+        $this->post_types['post'] = 'post';
+        $this->post_types['page'] = 'page';
+        ksort($this->post_types);
+        echo "<p>";
+        echo "<label for=\"" . $this->get_field_id('title') . "\">";
+        echo _e('Title:');
+        echo "</label>";
+        echo "<input class=\"widefat\" id=\"" . $this->get_field_id('title') . "\" name=\"" . $this->get_field_name('title') . "\" type=\"text\" value=\"" . $title . "\" />";
+        echo "</p>";
+        echo "<p>";
+        echo "<label for=\"" . $this->get_field_id('post_type') . "\">";
+        echo _e('Post Type:');
+        echo "</label>";
+        echo "<select name = \"" . $this->get_field_name('post_type') . "\" id=\"" . $this->get_field_id('title') . "\" >";
+        foreach ($this->post_types as $key => $post_type) {
+            echo '<option value="' . $key . '"' . ($key == $type ? " selected" : "") . '>' . $key . "</option>";
+        }
+        
+        echo "</select>";
+        echo "</p>";
+        echo "<p>";
+        echo "<label for=\"" . $this->get_field_id('num') . "\">";
+        echo _e('Number To show:');
+        
+        echo "</label>";
+        echo "<input id = \"" . $this->get_field_id('num') . "\" class = \"widefat\" name = \"" . $this->get_field_name('num') . "\" type=\"text\" value =\"" . $num . "\" / >";
+        echo "</p>";
+    }
+    function update($new_instance, $old_instance) {
+        $instance = $old_instance;
+        $instance['title'] = strip_tags($new_instance['title']);
+        $instance['num'] = (int)strip_tags($new_instance['num']);
+        $instance['post_type'] = strip_tags($new_instance['post_type']);
+        if ($instance['num'] < 1) {
+            $instance['num'] = 10;
+        }
+        return $instance;
+    }
+    function widget($args, $instance) {
+        extract($args);
+        $title = apply_filters('widget_title', $instance['title']);
+        echo $before_widget;
+        if ($title) {
+            echo $before_title . $title . $after_title;
+        }
+        echo "<ul class=\"widget-list featured-post-widget featured-post\">";
+        wp_reset_query();
+        global $wp_query;
+        $old_query = $wp_query;
+        $FeaturedPost_query = new WP_Query(array(
+            'post_type' => $instance['post_type'],
+            'showposts' => $instance['num'],
+            'featured' => 'yes',
+            'paged' => 1
+        ));
+        while ($FeaturedPost_query->have_posts()) {
+            $FeaturedPost_query->the_post();
+            echo "<li><a href=\"" . get_permalink() . "\">";
+            echo get_the_title();
+            echo "</a>";
+            echo "</li>";
+        }
+        wp_reset_query();
+        $wp_query = $old_query;
+        echo "</ul>";
+        echo $after_widget;
+        // outputs the content of the widget
+    }
 }
-add_action( 'widgets_init', create_function( '', 'return register_widget("Featured_Post_Widget");' ) );
+$Featured_Post = new Featured_Post();
 
+add_action('widgets_init', create_function('', 'return register_widget("Featured_Post_Widget");') , 100);
